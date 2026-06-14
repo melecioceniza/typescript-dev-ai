1fd85772de97dce15bd5593878dc8e16eb7d1f95
melecioceniza <melecio.ceniza@gmail.com>
2026-06-14 12:54:18 +1200

    add environment settings

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let number = 10;
+Math.round(number);
+let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let number = 10
+Math.round(number)
+let age: number = 20
+if (age < 50)
+    age += 10
+
+
+console.log(age)
